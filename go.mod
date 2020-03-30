c378bd06897ca201fae3cdb55a7e2567d9e4b716
Julien Vehent <julien@linuxwall.info>
2020-03-30 14:53:55 -0400

    Move sops decryption from helper to internal function

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/xlzd/gotp v0.0.0-20181030022105-c8557ba2c119
-	go.mozilla.org/sops v0.0.0-20190912205235-14a22d7a7060
+	go.mozilla.org/sops/v3 v3.5.0
+	google.golang.org/grpc v1.22.1
 	gopkg.in/yaml.v2 v2.2.2
 )
--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"time"
 
 	"github.com/xlzd/gotp"
-	"go.mozilla.org/sops/v3/decrypt"
+	"go.mozilla.org/sops/v3/aes"
+	"go.mozilla.org/sops/v3/cmd/sops/common"
+	"go.mozilla.org/sops/v3/cmd/sops/formats"
+	"go.mozilla.org/sops/v3/keyservice"
+	"google.golang.org/grpc"
+	"gopkg.in/yaml.v2"
 )
 
 type Config struct {
@@ -22,8 +29,7 @@ func main() {
 		fmt.Println("usage: sotp <account_name>")
 		os.Exit(1)
 	}
-	var cfg Config
-	err := decrypt.YamlFile("config.yaml", &cfg)
+	cfg, err := decryptConfig("config.yaml")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -42,4 +48,62 @@ func main() {
 	otp := gotp.NewDefaultTOTP(totpSecret)
 
 	fmt.Println("current one-time password is:", otp.Now())
+}
+
+func decryptConfig(path string) (cfg Config, err error) {
+	// Read the file into an []byte
+	encryptedData, err := ioutil.ReadFile(path)
+	if err != nil {
+		return cfg, fmt.Errorf("failed to read file %q: %w", path, err)
+	}
+
+	var svcs []keyservice.KeyServiceClient
+	svcs = append(svcs, keyservice.NewLocalClient())
+	// try connecting to unix:///tmp/sops.sock
+	conn, err := grpc.Dial("unix:///tmp/sops.sock", []grpc.DialOption{grpc.WithInsecure()}...)
+	if err == nil {
+		// ignore errors but only add the keyservice if the dial call succeded
+		svcs = append(svcs, keyservice.NewKeyServiceClient(conn))
+	}
+
+	store := common.StoreForFormat(formats.Yaml)
+
+	// Load SOPS file and access the data key
+	tree, err := store.LoadEncryptedFile(encryptedData)
+	if err != nil {
+		return cfg, err
+	}
+	key, err := tree.Metadata.GetDataKeyWithKeyServices(svcs)
+	if err != nil {
+		return cfg, err
+	}
+
+	// Decrypt the tree
+	cipher := aes.NewCipher()
+	mac, err := tree.Decrypt(key, cipher)
+	if err != nil {
+		return cfg, err
+	}
+
+	// Compute the hash of the cleartext tree and compare it with
+	// the one that was stored in the document. If they match,
+	// integrity was preserved
+	originalMac, err := cipher.Decrypt(
+		tree.Metadata.MessageAuthenticationCode,
+		key,
+		tree.Metadata.LastModified.Format(time.RFC3339),
+	)
+	if originalMac != mac {
+		return cfg, fmt.Errorf("Failed to verify data integrity. expected mac %q, got %q", originalMac, mac)
+	}
+
+	cleartext, err := store.EmitPlainFile(tree.Branches)
+	if err != nil {
+		return cfg, fmt.Errorf("failed to decrypt file: %w", err)
+	}
+	err = yaml.Unmarshal(cleartext, &cfg)
+	if err != nil {
+		return cfg, fmt.Errorf("failed to unmarshal cleartext into yaml: %w", err)
+	}
+	return
 }
